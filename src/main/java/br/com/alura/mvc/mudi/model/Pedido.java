67b998066120902f767d8dbea95e93e149558a97
Gui182 <guilherme.oliveira182@yahoo.com.br>
2023-03-19 23:28:13 -0300

    19/03 noite

---
+++ src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
@@ -0,0 +1,29 @@
+package br.com.alura.mvc.mudi.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Pedido {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String nomeProduto;
+    private BigDecimal valorNegociado;
+    private LocalDate dataDaEntrega;
+    private String urlProduto;
+    private String urlImagem;
+    private String descricao;
+
+}
